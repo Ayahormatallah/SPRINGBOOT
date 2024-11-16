4900f26fe145e4167bcdd4c10b32019e8fc0bf23
Ayahormatallah <hormatallah5@gmail.com>
2024-11-16 21:10:04 +0100

    Add files via upload

---
+++ student-management/student-management/src/test/java/com/example/student_management/StudentControllerTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.student_management;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+
+  public class StudentControllerTestApplication {
+
+
+        @Test
+        void contextLoads() {
+        }
+
+    }
+
+
